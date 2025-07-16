716d887acaef35207187d0903cf4c9963e47bc22
aastha-vij <vijaastha96@gmail.com>
2025-07-16 14:16:04 +0530

    sortArray

---
+++ src/main/java/coreJava/_006_SortArray.java
@@ -0,0 +1,28 @@
+package coreJava;
+
+public class _006_SortArray {
+
+	public static void sortArray(int[] arr) {
+		int temp=0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[i]>arr[j]) {
+				
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		int [] array = {5,7,33,6,8,1,-7} ;
+		sortArray(array);
+	}
+}
