71196f38d13f06d0f1084905a9a438d6d357cc91
aastha-vij <vijaastha96@gmail.com>
2025-07-19 21:00:14 +0530

    abstract class

---
+++ src/main/java/coreJava/_013_AbstractParentAirCraft.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public abstract class _013_AbstractParentAirCraft {
+
+	public void engine() {
+		System.out.println("Engine Guidelines");
+	}
+	
+	public void safety() {
+		System.out.println("Safety Guidelines");
+	}
+	
+	public abstract void color(); // Partial Abstraction: Method with no Body 
+
+}
