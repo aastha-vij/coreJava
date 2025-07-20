02bb67bec14e14dda38cb19de15435c43e189bbb
aastha-vij <vijaastha96@gmail.com>
2025-07-20 20:39:53 +0530

    Inheritance Parent Class

---
+++ src/main/java/coreJava/_015_InheritanceParent.java
@@ -0,0 +1,25 @@
+package coreJava;
+
+public class _015_InheritanceParent {
+
+	public void gear() {
+		System.out.println("Gear implementation");
+	}
+	
+	public void breaks() {
+		System.out.println("Break implementation");
+	}
+	
+	public void engine() {
+		System.out.println("Engine implementation");
+	}
+	
+	public void audio() {
+		System.out.println("Audio implementation");
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
