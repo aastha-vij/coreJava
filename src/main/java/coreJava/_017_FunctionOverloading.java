30f65827639d8354350e21a561f3bf667d4a407e
aastha-vij <vijaastha96@gmail.com>
2025-07-21 20:37:51 +0530

    Function Overloading

---
+++ src/main/java/coreJava/_017_FunctionOverloading.java
@@ -0,0 +1,35 @@
+package coreJava;
+
+public class _017_FunctionOverloading{
+	
+	/* 1. Overloading: Method Name is same
+	 * 2. To remove duplicate method, either:
+	 * 		- Argument count should be different
+	 * 		- Argument data type should be different
+	 * 		
+	 */
+
+	public void getData(int a) {
+		System.out.println(a);
+	}
+	
+	public void getData(String a) {
+		System.out.println(a);
+
+	}
+	
+	public void getData(int a, int b) {
+		System.out.println(a+ ","+ b);
+
+	}
+	
+	public static void main(String[] args) {
+		_017_FunctionOverloading obj = new _017_FunctionOverloading();
+		obj.getData(5);
+		obj.getData("Str");
+		obj.getData(5, 6);
+
+	}
+
+
+}
