ce1df392097aba9219abba8b086866a812c572c0
aastha-vij <vijaastha96@gmail.com>
2025-07-22 21:59:43 +0530

    Parametarized Constructor

--- src/main/java/coreJava/_020_Constructor.java
+++ src/main/java/coreJava/_020_Constructor.java
@@ -2,17 +2,25 @@ package coreJava;
 
 public class _020_Constructor {
 	
-	public _020_Constructor(){
+	public _020_Constructor() { 
 		
 		/* Block of code when object is created
 		 * Does not return any value
 		 * Constructor Name is always same as Class Name
 		 */
 		
-		System.out.println("Constructor");
+		System.out.println("Default Constructor");
 	}
 	
+	public _020_Constructor(int a, int b) { 
+		int c = a+b;
+		System.out.println("Parametarized Constructor: " + c);
+	}
 
+	public _020_Constructor(String a) { 
+		System.out.println("Parametarized Constructor: " + a);
+	}
+	
 	public void getData() {
 		System.out.println("Method getData");
 	}
@@ -21,5 +29,8 @@ public class _020_Constructor {
 	public static void main(String[] args) {
 
 		_020_Constructor obj  = new _020_Constructor(); // When object is created, constructor is called
+		_020_Constructor obj2  = new _020_Constructor(5, 6); // Parametarized Constructor
+		_020_Constructor obj3  = new _020_Constructor(""); // Parametarized Constructor
+
 	}
 }
