a6a4f66cf5d99716e7421c5e698e5655a0c55025
aastha-vij <vijaastha96@gmail.com>
2025-07-25 21:35:10 +0530

    static variable

---
+++ src/main/java/coreJava/_021_StaticVariables.java
@@ -0,0 +1,39 @@
+package coreJava;
+
+public class _021_StaticVariables {
+
+	// Instance variables
+	String name;
+	String address;
+	static String city = "Some City";
+	
+	public _021_StaticVariables(String name, String address) {
+		// Local variables --> name, address
+		// Instance variables --> this.name, this.address
+		
+		this.name = name;
+		this.address = address;
+	}
+	
+	public void getAddress() {
+		System.out.println(address + " "+ city);
+	}
+	
+	public void getName() {
+		System.out.println(name);
+	}
+
+	
+	public static void main(String[] args) {
+
+		_021_StaticVariables obj = new _021_StaticVariables("Some Name", "Some Address");
+		obj.getName();
+		obj.getAddress();
+
+		_021_StaticVariables obj2 = new _021_StaticVariables("Some Other Name", "Some Other Address");
+		obj2.getName();
+		obj2.getAddress();
+
+	}
+
+}
