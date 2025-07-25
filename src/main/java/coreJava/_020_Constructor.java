c216a52e200a51c8593e9b6d36b6759ef1fd94e1
aastha-vij <vijaastha96@gmail.com>
2025-07-25 20:54:18 +0530

    this keyword implementation

--- src/main/java/coreJava/_020_Constructor.java
+++ src/main/java/coreJava/_020_Constructor.java
@@ -27,10 +27,21 @@ public class _020_Constructor {
 	
 	////////////////////////this Keyword///////////////////////
 	
-	int value = 5;
+	int value = 5; // global variable
+	
 	public void getData(int value) {
 		this.value= value; // 'this.value' refers to the instance variable
 		System.out.println(value); // 'value' refers to the constructor parameter
+	}
+	
+	public void getDataThis() {
+		int value = 3; // local variable
+		System.out.println(value); 
+		System.out.println(this.value); //this refers to current object - scope is class level
+		
+		int globalLocalSum = value+ this.value;
+		System.out.println("globalLocalSum: "+globalLocalSum);
+
 	}
 	
 	///////////////////////////this Keyword///////////////////////
@@ -40,5 +51,6 @@ public class _020_Constructor {
 		_020_Constructor obj  = new _020_Constructor(); // When object is created, constructor is called
 		_020_Constructor obj2  = new _020_Constructor(5, 6); // Parametarized Constructor
 		_020_Constructor obj3  = new _020_Constructor("Hello"); // Parametarized Constructor
+		obj.getDataThis();
 	}
 }
