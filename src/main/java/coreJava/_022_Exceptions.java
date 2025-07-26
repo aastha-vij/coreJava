ffb0c10871bf60e541e0791e59c6eddb32a51598
aastha-vij <vijaastha96@gmail.com>
2025-07-26 20:23:48 +0530

    IndexOutOfBoundsException and ArithmeticException

--- src/main/java/coreJava/_022_Exceptions.java
+++ src/main/java/coreJava/_022_Exceptions.java
@@ -3,12 +3,29 @@ package coreJava;
 public class _022_Exceptions {
 	
 	public static void main(String[] args) {
-
+		
 		try {
+			
+			int a[] = {1,2,3,4};
+			System.out.println(a[6]);
+			 
 			System.out.println(4/0);
+
 		}
+		
+		// one try can be followed by multiple catch blocks
+		// catch should be immediate after try
+		
+		catch(IndexOutOfBoundsException iob) {
+			System.out.println("Out of Bound: "+iob); //java.lang.ArrayIndexOutOfBoundsException: Index 6 out of bounds for length 4
+		}
+		
+		catch(ArithmeticException ae){
+			System.out.println("Arithmetic: "+ae); // java.lang.ArithmeticException: / by zero
+		}
+		
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println("Generic: "+e); 
 		}
 	}
 
