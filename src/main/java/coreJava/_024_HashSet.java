6733193565908b63daf143f9ae899b26e1c9c168
aastha-vij <vijaastha96@gmail.com>
2025-07-28 23:52:51 +0530

    HashSet methods

--- src/main/java/coreJava/_024_HashSet.java
+++ src/main/java/coreJava/_024_HashSet.java
@@ -1,5 +1,7 @@
 package coreJava;
 
+import java.util.HashSet;
+
 public class _024_HashSet {
 
 	 /* Set Collection - HashSet, TreeSet, LinkedHashSet: Doesn't accept duplicates
@@ -7,6 +9,14 @@ public class _024_HashSet {
 	 */
 	public static void main(String[] args) {
 
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("test01");
+		hs.add("test02");
+		hs.add("test02"); // duplicates are removed
+		System.out.println(hs); // [test02, test01] --> No order
+		System.out.println(hs.remove("test02"));
+		System.out.println(hs.isEmpty());
+		System.out.println(hs.size());
 	}
 
 }
