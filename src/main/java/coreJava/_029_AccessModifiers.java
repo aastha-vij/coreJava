fe4c5f28e7557291e6a41b6c684e56d16bd155df
aastha-vij <vijaastha96@gmail.com>
2025-08-06 21:28:41 +0530

    Access Modifiers- Default and Public

---
+++ src/main/java/coreJava/_029_AccessModifiers.java
@@ -0,0 +1,17 @@
+package coreJava;
+
+final class _029_AccessModifiers { 
+
+	// Default: Access anywhere only in package 
+	void getData() { 
+	}
+	
+	int a = 0;
+	
+	// Public: Access anywhere inside/outside package 
+	public void getData(int a) { 
+	}
+	
+	public int b = 0;
+
+}
