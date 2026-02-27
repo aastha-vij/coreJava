28c9b402923c6ee4bee80e3365c4ad3b61e14090
aastha-vij <vijaastha96@gmail.com>
2026-02-27 11:48:16 +0400

    _001_CommonElementsInArray -> swap _02_commonItems if array1 is greater

--- src/main/java/InterviewQuestions/_001_CommonElementsInArray.java
+++ src/main/java/InterviewQuestions/_001_CommonElementsInArray.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 public class _001_CommonElementsInArray {
 
 	public static void main(String[] args) {
-        int[] array1 = {4, 3, 2};
-        int[] array2 = {2, 1, 7, 4};
+        int[] array2 = {4, 3, 2, 6, 77};
+        int[] array1 = {2, 1, 7, 4};
         
         _01_BFA(array1, array2);
         _02_commonItems(array1, array2);
@@ -19,7 +19,7 @@ public class _001_CommonElementsInArray {
 		for (int i = 0; i < array1.length; i++) {
 			for (int j = 0; j < array2.length; j++) {
 				if(array1[i]==array2[j])
-					System.out.print(array1[i]+" ");
+					System.out.print(array1[i]+ " ");
 			}
 		}
 	}
@@ -28,9 +28,11 @@ public class _001_CommonElementsInArray {
 		// Time Complexity: O(n+m) if array1.length!=array2.length, else O(n)
 		// Space Complexity: O(min(n,m)) if array1.length==array2.length O(n)
 		
-		if(array1.length>array2.length)
-			_02_commonItems(array2, array1);
-		
+		if(array1.length>array2.length) {
+	        int[] temp = array1;
+	        array1 = array2;
+	        array2 = temp;
+		}		
 		HashMap<Integer, Boolean> hm = new HashMap<>();
 		for (int i : array1)
 			hm.put(i, true);
