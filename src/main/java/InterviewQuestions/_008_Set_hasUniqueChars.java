85be55fa7a09d8f3fa3c0ea0d948187d8009e168
aastha-vij <vijaastha96@gmail.com>
2026-03-03 14:15:44 +0400

    _008_Set_hasUniqueChars -> _02_UsingHashmap

--- src/main/java/InterviewQuestions/_008_Set_hasUniqueChars.java
+++ src/main/java/InterviewQuestions/_008_Set_hasUniqueChars.java
@@ -1,5 +1,7 @@
 package InterviewQuestions;
 
+import java.util.HashMap;
+
 public class _008_Set_hasUniqueChars {
 
 	public static void main(String[] args) {
@@ -8,17 +10,35 @@ public class _008_Set_hasUniqueChars {
 		
 		System.out.println(_01_BFA(str1));
 		System.out.println(_01_BFA(str2));
-
+		
+		System.out.println(_02_UsingHashmap(str1));
+		System.out.println(_02_UsingHashmap(str2));
 	}
 
 	static boolean _01_BFA(String str) {
-		
+		// Time: O(n^2) 
+		// Space: O(1)
+
 		for (int i = 0; i < str.length(); i++) {
 			for (int j = 0; j < str.length(); j++) {
 				if(i!=j && str.charAt(i)==str.charAt(j))
 					return false;
 			}
 		}
+		return true;
+	}
+	
+	static boolean _02_UsingHashmap(String str) {
+		// Time: O(n)
+		// Space: O(n)
+		
+		HashMap<Character, Integer> hm = new HashMap<>();
+		for (char ch : str.toCharArray())
+			hm.put(ch, hm.getOrDefault(ch, 0)+1);
+		
+		for (char ch : str.toCharArray())
+			if(hm.get(ch)>1)
+				return false;
 		return true;
 	}
 }
