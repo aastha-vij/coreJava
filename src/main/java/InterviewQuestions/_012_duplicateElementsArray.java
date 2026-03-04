817529ca0630f797c559ad4a706e579b455f8833
aastha-vij <vijaastha96@gmail.com>
2026-03-04 19:51:44 +0400

    _012_duplicateElementsArray -> _02_UsingHashMap

--- src/main/java/InterviewQuestions/_012_duplicateElementsArray.java
+++ src/main/java/InterviewQuestions/_012_duplicateElementsArray.java
@@ -1,6 +1,7 @@
 package InterviewQuestions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class _012_duplicateElementsArray {
@@ -8,6 +9,7 @@ public class _012_duplicateElementsArray {
 	public static void main(String[] args) {
 		int[] arr = { 2, 10, 10, 100, 2, 10, 11, 2, 11, 2 }; // 2 10 11
 		System.out.println(_01_BFA(arr));
+		System.out.println(_02_UsingHashMap(arr));
 	}
 
 	static List<Integer> _01_BFA(int[] arr) {
@@ -23,6 +25,23 @@ public class _012_duplicateElementsArray {
 						duplicates.add(arr[i]);
 				}
 			}
+		}
+		return duplicates;
+	}
+	
+	static List<Integer> _02_UsingHashMap(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		
+		List<Integer> duplicates = new ArrayList<>();
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		
+		for (int i : arr)
+			hm.put(i, hm.getOrDefault(i, 0)+1);
+		
+		for(int key: hm.keySet()) {
+			if(hm.get(key)>1)
+				duplicates.add(key);
 		}
 		return duplicates;
 	}
