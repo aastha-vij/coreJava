bd8b45ce068d7810438ab64697cce8269e8811b1
aastha-vij <vijaastha96@gmail.com>
2026-03-04 10:09:51 +0400

    _009_Set_findPairsFromTwoArrays -> _02_UsingHashset

--- src/main/java/InterviewQuestions/_009_Set_findPairsFromTwoArrays.java
+++ src/main/java/InterviewQuestions/_009_Set_findPairsFromTwoArrays.java
@@ -2,7 +2,9 @@ package InterviewQuestions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class _009_Set_findPairsFromTwoArrays {
 
@@ -13,6 +15,11 @@ public class _009_Set_findPairsFromTwoArrays {
 		
 		List<int[]> result = _01_BFA(arr1, arr2, target);
 		for (int[] pair : result) {
+			System.out.print(Arrays.toString(pair));
+		}
+		
+		List<int[]> result2 = _02_UsingHashset(arr1, arr2, target);
+		for (int[] pair : result2) {
 			System.out.print(Arrays.toString(pair));
 		}
 	}
@@ -27,6 +34,22 @@ public class _009_Set_findPairsFromTwoArrays {
 				if(arr1[i]+arr2[j]== target)
 					pairs.add(new int[] {arr1[i], arr2[j]});
 			}
+		}
+		return pairs;
+	}
+	
+	static List<int[]> _02_UsingHashset(int[] arr1, int[] arr2, int target){
+		// Time Complexity: O(n + m)
+		// Space Complexity: O(n + m)
+		
+		Set<Integer> hs = new HashSet<>();
+		List<int[]> pairs = new ArrayList<>();
+		for (int num : arr1) {
+			hs.add(num);
+		}
+		for (int num : arr2) {
+			if(hs.contains(target-num))
+				pairs.add(new int[] {target-num, num});
 		}
 		return pairs;
 	}
