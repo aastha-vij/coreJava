96e5e444316ed72ee6f1c758865c4bd60f23856d
aastha-vij <vijaastha96@gmail.com>
2026-03-04 19:16:22 +0400

    _011_frequencyOfEachElementArray -> _01_BFA

---
+++ src/main/java/InterviewQuestions/_011_frequencyOfEachElementArray.java
@@ -0,0 +1,28 @@
+package InterviewQuestions;
+
+public class _011_frequencyOfEachElementArray {
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 20, 10, 10, 20, 5, 20 }; // 10: 3, 20: 4, 5: 1
+		_01_BFA(arr);
+	}
+
+	static void _01_BFA(int[] arr) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(n)
+		
+		boolean[] visited = new boolean[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			if(!visited[i]) {
+				int count = 1;
+				for (int j = i+1; j < arr.length; j++) {
+					if(arr[i]==arr[j]) {
+						visited[j] = true;
+						count++;
+					}
+				}
+				System.out.println(arr[i] + " "+ count);	
+			}
+		}
+	}
+}
