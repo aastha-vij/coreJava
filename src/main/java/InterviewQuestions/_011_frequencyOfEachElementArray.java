a4fb0ca924449257e2a60c5803d2fb515860d5f4
aastha-vij <vijaastha96@gmail.com>
2026-03-04 19:26:33 +0400

    _011_frequencyOfEachElementArray -> _02_UsingHashMap

--- src/main/java/InterviewQuestions/_011_frequencyOfEachElementArray.java
+++ src/main/java/InterviewQuestions/_011_frequencyOfEachElementArray.java
@@ -1,10 +1,13 @@
 package InterviewQuestions;
 
+import java.util.HashMap;
+
 public class _011_frequencyOfEachElementArray {
 
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 20, 10, 10, 20, 5, 20 }; // 10: 3, 20: 4, 5: 1
 		_01_BFA(arr);
+		System.out.println(_02_UsingHashMap(arr));
 	}
 
 	static void _01_BFA(int[] arr) {
@@ -24,5 +27,15 @@ public class _011_frequencyOfEachElementArray {
 				System.out.println(arr[i] + " "+ count);	
 			}
 		}
+	}
+	
+	static HashMap<Integer, Integer> _02_UsingHashMap(int[]arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int i : arr)
+				hm.put(i, hm.getOrDefault(i, 0)+1);
+		return hm;
 	}
 }
