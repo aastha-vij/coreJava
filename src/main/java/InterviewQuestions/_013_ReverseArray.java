ea084112e6abbb5f53819e0762de7c4046a2470b
aastha-vij <vijaastha96@gmail.com>
2026-03-05 09:15:16 +0400

    _013_ReverseArray -> _02_UsingStack

--- src/main/java/InterviewQuestions/_013_ReverseArray.java
+++ src/main/java/InterviewQuestions/_013_ReverseArray.java
@@ -1,6 +1,7 @@
 package InterviewQuestions;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class _013_ReverseArray {
 
@@ -8,6 +9,11 @@ public class _013_ReverseArray {
 		int[] arr = new int[] { 1, 2, 3, 4, 5 }; // 5, 4, 3, 2, 1
 		
 		Arrays.stream(_01_BFA(arr)).forEach(value-> System.out.print(value+" "));
+		System.out.println();
+		
+		Stack<Integer> reversedStack = _02_UsingStack(arr);
+		while(!reversedStack.empty())
+			System.out.print(reversedStack.pop()+" ");
 	}
 
 	static int[] _01_BFA(int[] arr) {
@@ -19,5 +25,15 @@ public class _013_ReverseArray {
 		for (int i = 0; i < arr.length; i++)
 			reverseArray[i] = arr[arr.length-1-i];
 		return reverseArray;
+	}
+	
+	static Stack<Integer> _02_UsingStack(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		
+		Stack<Integer> st = new Stack<>();
+		for (int num : arr)
+			st.push(num);
+		return st;
 	}
 }
