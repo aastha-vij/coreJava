b12e5cc2eec8594b30f1df457a01b45fcdfbc96f
aastha-vij <vijaastha96@gmail.com>
2026-03-05 11:24:39 +0400

    _014_largestElementArray -> _02_BFA

--- src/main/java/InterviewQuestions/_014_largestElementArray.java
+++ src/main/java/InterviewQuestions/_014_largestElementArray.java
@@ -2,8 +2,9 @@ package InterviewQuestions;
 public class _014_largestElementArray {
 
 	public static void main(String[] args) {
-		int[] arr = { 2, 10, 10, 100, 2, 10, 11, 2, 11, 2 }; // 100
+		int[] arr = { 10, 10, 10, 100, 2, 10, 11, 2, 11, 2 }; // 100
 		System.out.println(_01_BFA(arr));
+		System.out.println(_02_BFA(arr));
 	}
 	
 	static int _01_BFA(int[] arr) {
@@ -20,5 +21,23 @@ public class _014_largestElementArray {
 			}
 		}
 		return arr[arr.length-1];
+	}
+
+	static int _02_BFA(int[] arr) {
+		// Time: O(n^2)
+		// Space: O(1)
+
+		for (int i = 0; i < arr.length; i++) {
+			boolean largest = true;
+			for (int j = 0; j < arr.length; j++) {
+				if(arr[i] < arr[j]){
+					largest = false;
+					break;
+				}
+			}
+			if(largest)
+				return arr[i];
+		}
+		return -1;
 	}
 }
