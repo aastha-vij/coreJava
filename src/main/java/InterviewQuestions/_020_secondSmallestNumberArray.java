e408bb6219fb3cabc5e8b49e3106ca02704216d3
aastha-vij <vijaastha96@gmail.com>
2026-03-06 16:48:22 +0400

    _020_secondSmallestNumberArray -> _02_UsingArraySort

--- src/main/java/InterviewQuestions/_020_secondSmallestNumberArray.java
+++ src/main/java/InterviewQuestions/_020_secondSmallestNumberArray.java
@@ -1,9 +1,14 @@
 package InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _020_secondSmallestNumberArray {
     public static void main(String[] args){
         int[] arr1 = { 3, 100, 10, 100, 2, 10, 11, 2, 11, 2 }; // 3
         System.out.println(_01_UsingLinearScan(arr1));
+
+        int[] arr2 = { 500, 100, 10, 50, 300 }; // 50
+        System.out.println(_02_UsingArraySort(arr2));
     }
 
     static int _01_UsingLinearScan(int[] arr1){
@@ -20,5 +25,17 @@ public class _020_secondSmallestNumberArray {
                 secondSmallest = num;
         }
         return  secondSmallest;
+    }
+
+    static int _02_UsingArraySort(int[] arr){
+        // Time complexity: O(nlogn)
+        // Space complexity: O(1)
+
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]!=arr[i+1])
+                return arr[i+1];
+        }
+        return 0;
     }
 }
