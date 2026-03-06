964d5a6979f485ecafe10d745e6d74015c902b4b
aastha-vij <vijaastha96@gmail.com>
2026-03-06 12:41:58 +0400

    _017_SumItemsInArray -> _02_UsingWhileLoop

--- src/main/java/InterviewQuestions/_017_SumItemsInArray.java
+++ src/main/java/InterviewQuestions/_017_SumItemsInArray.java
@@ -5,6 +5,7 @@ public class _017_SumItemsInArray {
     public static void main(String[] args) {
         int[] arr = { 15, 12, 13, 10 }; // 50
         System.out.println(_01_UsingForLoop(arr));
+        System.out.println(_02_UsingWhileLoop(arr));
     }
 
     static int _01_UsingForLoop(int[] arr){
@@ -14,6 +15,19 @@ public class _017_SumItemsInArray {
         int sum = 0;
         for (int num : arr)
             sum += num;
+        return sum;
+    }
+
+    static int _02_UsingWhileLoop(int[] arr){
+        // Time complexity: O(n)
+        // Space complexity: O(1)
+
+        int sum = 0;
+        int i = 0;
+        while(i<arr.length){
+            sum+=arr[i];
+            i++;
+        }
         return sum;
     }
 }
