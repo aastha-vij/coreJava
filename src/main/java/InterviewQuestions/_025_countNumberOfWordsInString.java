3c072857ecf308a098014db33560f2545e80ddaf
aastha-vij <vijaastha96@gmail.com>
2026-03-07 20:17:39 +0400

    _025_countNumberOfWordsInString -> _03_UsingStringTokenizer

--- src/main/java/InterviewQuestions/_025_countNumberOfWordsInString.java
+++ src/main/java/InterviewQuestions/_025_countNumberOfWordsInString.java
@@ -2,12 +2,14 @@ package InterviewQuestions;
 
 import java.sql.Array;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class _025_countNumberOfWordsInString {
     public static void main(String[] args) {
         String str = "    India Is My Country";
         System.out.println("Word Count using Spilt: " + _01_Using_Split_Array(str));
         System.out.println("Word Count using Stream: " + _02_UsingStream(str));
+        System.out.println("Word Count using StringTokenizer: " + _03_UsingStringTokenizer(str));
     }
 
     static int _01_Using_Split_Array(String str){
@@ -23,5 +25,13 @@ public class _025_countNumberOfWordsInString {
         // Space Complexity: O(n)
 
         return Arrays.stream(str.trim().split("\\s+")).count();
+    }
+
+    static int _03_UsingStringTokenizer(String str){
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        StringTokenizer st = new StringTokenizer(str);
+        return st.countTokens();
     }
 }
