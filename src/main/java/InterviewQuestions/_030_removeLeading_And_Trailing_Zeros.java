c7b607c8c7845bf61af718726075ed8c465f43f1
aastha-vij <vijaastha96@gmail.com>
2026-03-09 12:09:20 +0400

    _030_removeLeading_And_Trailing_Zeros -> _01_Using_subString

---
+++ src/main/java/InterviewQuestions/_030_removeLeading_And_Trailing_Zeros.java
@@ -0,0 +1,32 @@
+package InterviewQuestions;
+
+public class _030_removeLeading_And_Trailing_Zeros {
+    public static void main(String[] args) {
+        String str1 = "1230456000000"; // 1230456
+        String str2 = "00001230456000"; // 1230456
+
+        System.out.println(_01_Using_subString(str1));
+        System.out.println(_01_Using_subString(str2));
+    }
+
+    static String _01_Using_subString(String str) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) != '0'){
+                start = i;
+                break;
+            }
+        }
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            if(str.charAt(i) != '0'){
+                end = i;
+                break;
+            }
+        }
+        return str.substring(start, end+1);
+    }
+}
