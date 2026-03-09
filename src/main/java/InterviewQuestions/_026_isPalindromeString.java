6db1d108d5630ee965a41122fadb4dc84d8c4c4d
aastha-vij <vijaastha96@gmail.com>
2026-03-09 10:59:48 +0400

    _026_isPalindromeString -> _02_Using_Two_Pointer_Technique

--- src/main/java/InterviewQuestions/_026_isPalindromeString.java
+++ src/main/java/InterviewQuestions/_026_isPalindromeString.java
@@ -9,6 +9,10 @@ public class _026_isPalindromeString {
         System.out.println(_01_BFA(str1));
         System.out.println(_01_BFA(str2));
         System.out.println(_01_BFA(str3));
+
+        System.out.println(_02_Using_Two_Pointer_Technique(str1));
+        System.out.println(_02_Using_Two_Pointer_Technique(str2));
+        System.out.println(_02_Using_Two_Pointer_Technique(str3));
     }
 
     static boolean _01_BFA(String str){
@@ -20,5 +24,19 @@ public class _026_isPalindromeString {
         for (int i = str.length()-1; i >=0 ; i--)
             reverse.append(str.charAt(i));
         return str.contentEquals(reverse);
+    }
+
+    static boolean _02_Using_Two_Pointer_Technique(String str){
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        str = str.trim().replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = str.length()-1;
+        while(left< right){
+            if(str.charAt(left++)!=str.charAt(right--))
+                return false;
+        }
+        return true;
     }
 }
