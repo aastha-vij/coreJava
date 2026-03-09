18108ca3478f8b1f3ad7ebf3530a3e6d7ce59c0e
aastha-vij <vijaastha96@gmail.com>
2026-03-09 11:11:40 +0400

    _027_reverseString -> _02_Using_Stack

--- src/main/java/InterviewQuestions/_027_reverseString.java
+++ src/main/java/InterviewQuestions/_027_reverseString.java
@@ -1,9 +1,12 @@
 package InterviewQuestions;
 
+import java.util.Stack;
+
 public class _027_reverseString {
     public static void main(String[] args) {
         String str = "Geeks"; // skeeG
         System.out.println(_01_BFA(str));
+        System.out.println(_02_Using_Stack(str));
     }
 
     static String _01_BFA(String str){
@@ -15,5 +18,19 @@ public class _027_reverseString {
             reverse.append(str.charAt(i));
         }
         return reverse.toString();
+    }
+
+    static StringBuilder _02_Using_Stack(String str){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        StringBuilder reverse = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : str.toCharArray())
+            st.add(ch);
+        while(!st.empty())
+            reverse.append(st.pop());
+        return reverse;
     }
 }
