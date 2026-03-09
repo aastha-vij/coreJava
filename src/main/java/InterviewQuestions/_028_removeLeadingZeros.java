0cbf0c5f716a29bbca3b5f64e6bd3d66d29503d2
aastha-vij <vijaastha96@gmail.com>
2026-03-09 11:29:48 +0400

    _028_removeLeadingZeros -> _01_Using_Substring

---
+++ src/main/java/InterviewQuestions/_028_removeLeadingZeros.java
@@ -0,0 +1,21 @@
+package InterviewQuestions;
+
+public class _028_removeLeadingZeros {
+    public static void main(String[] args) {
+        String str1 = "00000123569";// 123569
+        String str2 = "000012356090";// 12356090
+        System.out.println(_01_Using_Substring(str1));
+        System.out.println(_01_Using_Substring(str2));
+    }
+
+    static String _01_Using_Substring(String str){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) != '0')
+                return str.substring(i);
+        }
+        return str;
+    }
+}
