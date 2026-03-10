03f1ef08b5babc2233cc8fa38c38c6a95b33e21c
aastha-vij <vijaastha96@gmail.com>
2026-03-10 17:08:10 +0400

    _031_firstLetterOfEachWordString -> _02_Using_String_Scan

--- src/main/java/InterviewQuestions/_031_firstLetterOfEachWordString.java
+++ src/main/java/InterviewQuestions/_031_firstLetterOfEachWordString.java
@@ -4,6 +4,8 @@ public class _031_firstLetterOfEachWordString {
     public static void main(String[] args) {
         String str = "  geeks for geeks   "; // gfg
         _01_BFA(str);
+        System.out.println();
+        System.out.print(_02_Using_String_Scan(str));
     }
 
     static void _01_BFA(String str){
@@ -14,5 +16,18 @@ public class _031_firstLetterOfEachWordString {
         for (String s : splitStr)
             System.out.print(s.charAt(0));
 
+    }
+    
+    static String _02_Using_String_Scan(String str){
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if((i==0 && str.charAt(i) != ' ') ||
+                    (i>0 && str.charAt(i-1)==' ' && str.charAt(i)!=' '))
+                sb.append(str.charAt(i));
+        }
+        return sb.toString();
     }
 }
