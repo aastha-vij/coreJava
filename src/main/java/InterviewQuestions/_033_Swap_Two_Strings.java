08425df285bd18eaa32fdd3516f3025b40db1bd2
aastha-vij <vijaastha96@gmail.com>
2026-03-11 17:20:16 +0400

    _033_Swap_Two_Strings -> _01_Without_Using_Temp_Variable

---
+++ src/main/java/InterviewQuestions/_033_Swap_Two_Strings.java
@@ -0,0 +1,22 @@
+package InterviewQuestions;
+
+public class _033_Swap_Two_Strings {
+    public static void main(String[] args) {
+        String s1 = "Aastha";
+        String s2 = "Vij";
+        _01_Without_Using_Temp_Variable(s1, s2);
+    }
+
+    static void _01_Without_Using_Temp_Variable(String str1, String str2){
+        // Time complexity: O(n)
+        // Space complexity: O(n)
+
+        System.out.println("Before Swap: "+ "S1: "+ str1 + " S2: "+str2);
+
+        str1 = str1 + str2; // AasthaVij
+        str2 = str1.substring(0, str1.length()-str2.length()); // Aastha
+        str1 = str1.substring(str2.length());
+
+        System.out.println("After Swap: "+ "S1: "+ str1 + " S2: "+str2);
+    }
+}
