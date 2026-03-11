fad90285446353f2f7070dd994be37802a5001ed
aastha-vij <vijaastha96@gmail.com>
2026-03-11 17:30:51 +0400

    _033_Swap_Two_Strings -> _03_Using_Array

--- src/main/java/InterviewQuestions/_033_Swap_Two_Strings.java
+++ src/main/java/InterviewQuestions/_033_Swap_Two_Strings.java
@@ -6,6 +6,7 @@ public class _033_Swap_Two_Strings {
         String s2 = "Vij";
         _01_Without_Using_Temp_Variable(s1, s2);
         _02_Using_Temp_Variable(s1, s2);
+        _03_Using_Array(s1, s2);
     }
 
     static void _01_Without_Using_Temp_Variable(String str1, String str2){
@@ -31,6 +32,17 @@ public class _033_Swap_Two_Strings {
         str1 = str2;
         str2 = temp;
 
+        System.out.println("After Swap: "+ "S1: "+ str1 + " S2: "+str2);
+    }
+
+    static void _03_Using_Array(String str1, String str2){
+        // Time complexity: O(1)
+        // Space complexity: O(1)
+
+        System.out.println("Before Swap: "+ "S1: "+ str1 + " S2: "+str2);
+        String[] arr = {str1, str2};
+        str2 =  arr[0];
+        str1 = arr[1];
         System.out.println("After Swap: "+ "S1: "+ str1 + " S2: "+str2);
     }
 }
