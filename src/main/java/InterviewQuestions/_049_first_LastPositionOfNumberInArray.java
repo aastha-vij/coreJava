6b030abf1ada5f6e119ae378018b4bace5f3e5e2
aastha-vij <vijaastha96@gmail.com>
2026-03-17 17:26:13 +0400

    _049_first_LastPositionOfNumberInArray -> _02_UsingLinearIteration

--- src/main/java/InterviewQuestions/_049_first_LastPositionOfNumberInArray.java
+++ src/main/java/InterviewQuestions/_049_first_LastPositionOfNumberInArray.java
@@ -7,9 +7,13 @@ public class _049_first_LastPositionOfNumberInArray {
         int[] arr = {5, 7, 7, 8, 8, 10};
         int target = 8; // Output: [3, 4]
         System.out.println(Arrays.toString(_01_BFA(arr, target)));
+        System.out.println(Arrays.toString(_02_UsingLinearIteration(arr, target)));
     }
 
     static int[] _01_BFA(int[] arr, int target){
+        // Time Complexity: O(n^2)
+        // Space Complexity: O(1)
+
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]==target) {
                 for (int j = i+1; j < arr.length ; j++) {
@@ -19,5 +23,20 @@ public class _049_first_LastPositionOfNumberInArray {
             }
         }
         return null;
+    }
+
+    static int[] _02_UsingLinearIteration (int[] arr, int target) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int first = -1, last = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                if (first == -1)
+                    first = i;
+                last = i;
+            }
+        }
+        return new int[]{first, last};
     }
 }
