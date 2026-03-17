bc0ac4aa2ba4dd1645dca1e90bbbeda7b623b5ee
aastha-vij <vijaastha96@gmail.com>
2026-03-17 17:00:33 +0400

    _047_missingNumberInGivenArray -> _02_Using_HashSet

--- src/main/java/InterviewQuestions/_047_missingNumberInGivenArray.java
+++ src/main/java/InterviewQuestions/_047_missingNumberInGivenArray.java
@@ -1,10 +1,13 @@
 package InterviewQuestions;
 
+import java.util.HashSet;
+
 public class _047_missingNumberInGivenArray {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 5, 6}; // 4
         System.out.println(_01_BFA(arr));
+        System.out.println(_02_Using_HashSet(arr));
     }
 
     static int _01_BFA( int[] arr ){
@@ -17,5 +20,19 @@ public class _047_missingNumberInGivenArray {
         for (int num : arr)
             actualSum+=num;
         return expectedSum - actualSum;
+    }
+
+    static int _02_Using_HashSet(int[] arr){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : arr)
+            hs.add(num);
+        for (int num : hs){
+            if(!hs.contains(num+1))
+                return num+1;
+        }
+        return 0;
     }
 }
