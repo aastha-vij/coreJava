73bd35ee7d83ed6aca9d5318c0abf23c8858be39
aastha-vij <vijaastha96@gmail.com>
2026-03-18 13:11:39 +0400

    _051_shiftAllZerosOnRight -> _01_BFA

---
+++ src/main/java/InterviewQuestions/_051_shiftAllZerosOnRight.java
@@ -0,0 +1,23 @@
+package InterviewQuestions;
+
+import java.util.Arrays;
+
+public class _051_shiftAllZerosOnRight {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 0, 2, 0, 3, 0, 0, 0}; // 1, 2, 3, 0, 0, 0, 0, 0
+        System.out.println(Arrays.toString(_01_BFA(arr1)));
+    }
+
+    static int[] _01_BFA(int[] arr ){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        int index = 0;
+        int[] shiftedArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0)
+                shiftedArr[index++] = arr[i];
+        }
+        return shiftedArr;
+    }
+}
