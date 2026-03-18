2710a7c3f4da19ff55195b26a22f0b71618caf00
aastha-vij <vijaastha96@gmail.com>
2026-03-18 13:17:47 +0400

    _051_shiftAllZerosOnRight -> _02_Using_InPlaceApproach

--- src/main/java/InterviewQuestions/_051_shiftAllZerosOnRight.java
+++ src/main/java/InterviewQuestions/_051_shiftAllZerosOnRight.java
@@ -6,6 +6,7 @@ public class _051_shiftAllZerosOnRight {
     public static void main(String[] args) {
         int[] arr1 = {1, 0, 2, 0, 3, 0, 0, 0}; // 1, 2, 3, 0, 0, 0, 0, 0
         System.out.println(Arrays.toString(_01_BFA(arr1)));
+        System.out.println(Arrays.toString(_02_Using_InPlaceApproach(arr1)));
     }
 
     static int[] _01_BFA(int[] arr ){
@@ -19,5 +20,20 @@ public class _051_shiftAllZerosOnRight {
                 shiftedArr[index++] = arr[i];
         }
         return shiftedArr;
+    }
+
+    static int[] _02_Using_InPlaceApproach(int[] arr){
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0){
+                arr[index++] = arr[i];
+            }
+        }
+        while(index < arr.length)
+            arr[index++] = 0;
+        return arr;
     }
 }
