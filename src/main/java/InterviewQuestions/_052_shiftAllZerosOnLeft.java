13b7e18632ab5beb2815b9ddfa8d872254b34d9c
aastha-vij <vijaastha96@gmail.com>
2026-03-18 13:34:00 +0400

    _052_shiftAllZerosOnLeft -> _02_Using_InPlaceApproach

--- src/main/java/InterviewQuestions/_052_shiftAllZerosOnLeft.java
+++ src/main/java/InterviewQuestions/_052_shiftAllZerosOnLeft.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class _052_shiftAllZerosOnLeft {
     public static void main(String[] args) {
         int[] arr1 = {1, 0, 2, 0, 3, 0, 0, 0}; // 0, 0, 0, 0, 0, 1, 2, 3
+        int[] arr2 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 0, 0, 0, 0, 0, 1, 2, 3
+
         System.out.println(Arrays.toString(_01_BFA(arr1)));
+        System.out.println(Arrays.toString(_02_Using_InPlaceApproach(arr2)));
     }
 
     static int[] _01_BFA (int[] arr){
@@ -19,5 +22,16 @@ public class _052_shiftAllZerosOnLeft {
                 shiftedArr[index--] = arr[i];
         }
         return shiftedArr;
+    }
+
+    private static int[] _02_Using_InPlaceApproach(int[] arr) {
+        int index = arr.length-1;
+        for (int i = arr.length-1; i >=0 ; i--) {
+            if(arr[i] != 0)
+                arr[index--] = arr[i];
+        }
+        while (index >=0)
+            arr[index--] = 0;
+        return arr;
     }
 }
