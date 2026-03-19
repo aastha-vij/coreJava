817580b97f99acc2bf69b59adf67775d0d3aed55
aastha-vij <vijaastha96@gmail.com>
2026-03-19 14:30:21 +0400

    _055_Reverse_First_Half_Array -> _01_BFA

---
+++ src/main/java/InterviewQuestions/_055_Reverse_First_Half_Array.java
@@ -0,0 +1,26 @@
+package InterviewQuestions;
+
+import java.util.Arrays;
+
+public class _055_Reverse_First_Half_Array {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7}; // {3,2,1,4,5,6,7}
+        System.out.println(Arrays.toString(_01_BFA(arr1)));
+    }
+
+    static int[] _01_BFA(int[] arr){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        int mid = arr.length/2-1;
+        int[] result = new int[arr.length];
+        int index = 0;
+        for (int i = mid; i >=0; i--) {
+            result[index++] = arr[i];
+        }
+        for (int i = mid+1; i <arr.length ; i++) {
+            result[index++] = arr[i];
+        }
+        return result;
+    }
+}
