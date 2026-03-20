2e6fc59d82b3fd86193f9d950fb4608df7ca22ac
aastha-vij <vijaastha96@gmail.com>
2026-03-20 11:38:13 +0400

    _055_Reverse_First_Half_Array -> _02_Using_Stack

--- src/main/java/InterviewQuestions/_055_Reverse_First_Half_Array.java
+++ src/main/java/InterviewQuestions/_055_Reverse_First_Half_Array.java
@@ -1,11 +1,17 @@
 package InterviewQuestions;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class _055_Reverse_First_Half_Array {
     public static void main(String[] args) {
         int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7}; // {3,2,1,4,5,6,7}
+        int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6 }; // {3,2,1,4,5,6}
         System.out.println(Arrays.toString(_01_BFA(arr1)));
+        System.out.println(Arrays.toString(_01_BFA(arr2)));
+
+        System.out.println(Arrays.toString(_02_Using_Stack(arr1)));
+        System.out.println(Arrays.toString(_02_Using_Stack(arr2)));
     }
 
     static int[] _01_BFA(int[] arr){
@@ -22,5 +28,19 @@ public class _055_Reverse_First_Half_Array {
             result[index++] = arr[i];
         }
         return result;
+    }
+
+    static int[] _02_Using_Stack(int[] arr){
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        int mid = arr.length/2-1;
+        int index = 0;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i <= mid; i++)
+            st.push(arr[i]);
+        while(!st.empty())
+            arr[index++] = st.pop();
+        return arr;
     }
 }
