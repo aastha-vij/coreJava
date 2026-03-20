0865d2f10573a4ff6270b3febd81f67f082b2330
aastha-vij <vijaastha96@gmail.com>
2026-03-20 11:44:23 +0400

    _055_Reverse_First_Half_Array -> _03_Using_Temp

--- src/main/java/InterviewQuestions/_055_Reverse_First_Half_Array.java
+++ src/main/java/InterviewQuestions/_055_Reverse_First_Half_Array.java
@@ -6,41 +6,58 @@ import java.util.Stack;
 public class _055_Reverse_First_Half_Array {
     public static void main(String[] args) {
         int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7}; // {3,2,1,4,5,6,7}
-        int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6 }; // {3,2,1,4,5,6}
+        int[] arr2 = new int[]{1, 2, 3, 4, 5, 6}; // {3,2,1,4,5,6}
         System.out.println(Arrays.toString(_01_BFA(arr1)));
         System.out.println(Arrays.toString(_01_BFA(arr2)));
 
         System.out.println(Arrays.toString(_02_Using_Stack(arr1)));
         System.out.println(Arrays.toString(_02_Using_Stack(arr2)));
+
+        System.out.println(Arrays.toString(_03_Using_Temp(arr1)));
+        System.out.println(Arrays.toString(_03_Using_Temp(arr2)));
     }
 
-    static int[] _01_BFA(int[] arr){
+    static int[] _01_BFA(int[] arr) {
         // Time Complexity: O(n)
         // Space Complexity: O(n)
 
-        int mid = arr.length/2-1;
+        int mid = arr.length / 2 - 1;
         int[] result = new int[arr.length];
         int index = 0;
-        for (int i = mid; i >=0; i--) {
+        for (int i = mid; i >= 0; i--) {
             result[index++] = arr[i];
         }
-        for (int i = mid+1; i <arr.length ; i++) {
+        for (int i = mid + 1; i < arr.length; i++) {
             result[index++] = arr[i];
         }
         return result;
     }
 
-    static int[] _02_Using_Stack(int[] arr){
+    static int[] _02_Using_Stack(int[] arr) {
         // Time Complexity: O(n)
         // Space Complexity: O(n)
 
-        int mid = arr.length/2-1;
+        int mid = arr.length / 2 - 1;
         int index = 0;
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i <= mid; i++)
             st.push(arr[i]);
-        while(!st.empty())
+        while (!st.empty())
             arr[index++] = st.pop();
+        return arr;
+    }
+
+    static int[] _03_Using_Temp(int[] arr) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int temp = 0;
+        int mid = arr.length/2-1;
+        for (int i = mid; i >= 0; i--) {
+            temp = arr[i];
+            arr[i] = arr[mid-i];
+            arr[mid-i] = temp;
+        }
         return arr;
     }
 }
